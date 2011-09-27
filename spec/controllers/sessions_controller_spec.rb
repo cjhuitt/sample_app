f9c6526cd76be7244ea43e4890836e3f44db906e
Caleb Huitt <git-caleb@huitt.org>
2011-09-26 19:32:50 -0500

    Handle signin failure

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -4,7 +4,6 @@ describe SessionsController do
   render_views
 
   describe "GET 'new'" do
-
     it "should be successful" do
       get :new
       response.should be_success
@@ -14,7 +13,29 @@ describe SessionsController do
       get :new
       response.should have_selector("title", :content => "Sign in")
     end
+  end
 
+  describe "POST 'create'" do
+    describe "invalid signin" do
+      before(:each) do
+        @attr = { :email => "email@example.com", :password => "invalid" }
+      end
+
+      it "should re-render the new page" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+
+      it "should have the right title" do
+        post :create, :session => @attr
+        response.should render_template('new')
+      end
+
+      it "should have a flash.now message" do
+        post :create, :session => @attr
+        flash.now[:error].should =~ /invalid/i
+      end
+    end
   end
 
 end
