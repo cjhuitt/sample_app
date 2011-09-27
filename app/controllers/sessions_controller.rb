b2ee2602d1cb2cb1d99d2453541d5c024840b91e
Caleb Huitt <git-caleb@huitt.org>
2011-09-26 19:36:40 -0500

    sign-in success tests

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,7 +12,8 @@ class SessionsController < ApplicationController
       @title = "Sign in"
       render 'new'
     else
-      #sign the user in and redireect to the user's show page
+      sign_in user
+      redirect_to user
     end
   end
 
