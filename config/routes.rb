e9a25d9e9e989d4ced72a9190ae8fc2815e33c69
Caleb Huitt <git-caleb@huitt.org>
2011-09-26 19:19:07 -0500

    routes for sessions

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ SampleApp::Application.routes.draw do
   get "sessions/new"
 
   resources :users
+  resources :sessions, :only => [:new, :create, :destroy]
 
   match '/home', :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   match '/signup', :to => 'users#new'
+
+  match '/signin', :to => 'sessions#new'
+  match '/signout', :to => 'sessions#destroy'
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
