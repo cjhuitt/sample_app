ac2c3f8fddb07dd6b7e60c67b07b5d6c7bf14979
Caleb Huitt <git-caleb@huitt.org>
2011-10-02 10:41:39 -0500

    correct user authorization checks

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -206,8 +206,25 @@ describe UsersController do
       end
 
       it "should deny access to 'update'" do
-        get :update, :id => @user, :user => {}
+        put :update, :id => @user, :user => {}
         response.should redirect_to(signin_path)
+      end
+    end
+
+    describe "for signed-in users" do
+      before(:each) do
+        wrong_user = Factory(:user,  :email => "user@example.net")
+        test_sign_in(wrong_user)
+      end
+
+      it "should should require matching users for 'edit'" do
+        get :edit, :id => @user
+        response.should redirect_to(root_path)
+      end
+
+      it "should should require matching users for 'update'" do
+        put :update, :id => @user, :user => {}
+        response.should redirect_to(root_path)
       end
     end
   end
