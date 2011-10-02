5b806444628717fa05c2bede8288f3f765d8c771
Caleb Huitt <git-caleb@huitt.org>
2011-10-02 11:26:32 -0500

    testing pagination

--- spec/factories.rb
+++ spec/factories.rb
@@ -5,3 +5,7 @@ Factory.define :user do |user|
   user.password               "passwd1"
   user.password_confirmation  "passwd1"
 end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
+end
