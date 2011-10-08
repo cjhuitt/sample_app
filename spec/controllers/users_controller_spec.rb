bad3cb7268ea59dc3c0b3cd8af50c4baffb1a35e
Caleb Huitt <git-caleb@huitt.org>
2011-10-08 09:55:33 -0500

    Showing microposts

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -106,7 +106,7 @@ describe UsersController do
     end
   end
 
-  describe "Get 'show'" do
+  describe "GET 'show'" do
     before(:each) do
       @user = Factory(:user)
     end
@@ -134,6 +134,14 @@ describe UsersController do
     it "should have a profile image" do
       get :show, :id => @user
       response.should have_selector("h1>img", :class => "gravatar")
+    end
+
+    it "should show the user's microposts" do
+      mp1 = Factory(:micropost, :user => @user, :content => "Foo bar")
+      mp2 = Factory(:micropost, :user => @user, :content => "Bax quux")
+      get :show, :id => @user
+      response.should have_selector("span.content", :content => mp1.content)
+      response.should have_selector("span.content", :content => mp2.content)
     end
   end
 
