9a3f36d07d4df019ccad3a625acc803c2bce17fe
Caleb Huitt <development-caleb@huitt.org>
2011-12-10 12:02:58 -0600

    Follow/unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,22 @@
 SampleApp::Application.routes.draw do
   get "sessions/new"
 
-  resources :users
-  resources :sessions, :only => [:new, :create, :destroy]
-  resources :microposts, :only => [:create, :destroy]
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :sessions,      :only => [:new, :create, :destroy]
+  resources :microposts,    :only => [:create, :destroy]
+  resources :relationships, :only => [:create, :destroy]
 
-  match '/home', :to => 'pages#home'
+  match '/home',    :to => 'pages#home'
   match '/contact', :to => 'pages#contact'
-  match '/about', :to => 'pages#about'
-  match '/help', :to => 'pages#help'
-  match '/signup', :to => 'users#new'
+  match '/about',   :to => 'pages#about'
+  match '/help',    :to => 'pages#help'
+  match '/signup',  :to => 'users#new'
 
-  match '/signin', :to => 'sessions#new'
+  match '/signin',  :to => 'sessions#new'
   match '/signout', :to => 'sessions#destroy'
 
   # You can have the root of your site routed with "root"
